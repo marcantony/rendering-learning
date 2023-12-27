4f60de63f103981ab318936ea961ea61eff70baf
Josh <5685731+marcantony@users.noreply.github.com>
2023-12-27 01:08:58 -0500

    Implement matrix multiply by matrix and tuple

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -1,4 +1,6 @@
-use super::util;
+use std::ops::Mul;
+
+use super::{util, tuple::Tuple3};
 
 #[derive(Debug)]
 pub struct Matrix<const N: usize>([[f64; N]; N]);
@@ -22,6 +24,46 @@ impl<const N: usize> PartialEq for Matrix<N> {
         assert_eq!(lhs_vals.len(), rhs_vals.len(), "Both matrices of size {} don't have the same number of elements.", N);
 
         lhs_vals.into_iter().zip(rhs_vals.into_iter()).all(|(x, y)| util::are_equal(x, y))
+    }
+}
+
+impl<const N: usize> Mul for &Matrix<N> {
+    type Output = Matrix<N>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut output = [[0.0; N]; N];
+
+        for n in 0..N {
+            for m in 0..N {
+                let mut sum = 0.0;
+                for i in 0..N {
+                    sum += self.0[n][i] * rhs.0[i][m];
+                }
+                output[n][m] = sum;
+            }
+        }
+
+        Matrix::new(output)
+    }
+}
+
+impl Mul<&Tuple3> for &Matrix<4> {
+    type Output = Tuple3;
+
+    fn mul(self, rhs: &Tuple3) -> Self::Output {
+        const N: usize = 4;
+
+        let mut output = [0.0; N];
+
+        for n in 0..N {
+            output[n] =
+                self.0[n][0] * rhs.x() +
+                self.0[n][1] * rhs.y() +
+                self.0[n][2] * rhs.z() +
+                self.0[n][3] * rhs.w();
+        }
+
+        Tuple3::new(output[0], output[1], output[2], output[3])
     }
 }
 
@@ -112,6 +154,50 @@ mod tests {
             ]);
 
             assert_ne!(a, b);
+        }
+    }
+
+    mod multiply {
+        use crate::math::tuple::Tuple3;
+
+        use super::*;
+
+        #[test]
+        fn multiply_two_matrices() {
+            let a = Matrix::new([
+                [1.0, 2.0, 3.0, 4.0],
+                [5.0, 6.0, 7.0, 8.0],
+                [9.0, 8.0, 7.0, 6.0],
+                [5.0, 4.0, 3.0, 2.0]
+            ]);
+
+            let b = Matrix::new([
+                [-2.0, 1.0, 2.0, 3.0],
+                [3.0, 2.0, 1.0, -1.0],
+                [4.0, 3.0, 6.0, 5.0],
+                [1.0, 2.0, 7.0, 8.0]
+            ]);
+
+            assert_eq!(&a * &b, Matrix::new([
+                [20.0, 22.0, 50.0, 48.0],
+                [44.0, 54.0, 114.0, 108.0],
+                [40.0, 58.0, 110.0, 102.0],
+                [16.0, 26.0, 46.0, 42.0]
+            ]));
+        }
+
+        #[test]
+        fn multiply_matrix_by_tuple() {
+            let a = Matrix::new([
+                [1.0, 2.0, 3.0, 4.0],
+                [2.0, 4.0, 4.0, 2.0],
+                [8.0, 6.0, 4.0, 1.0],
+                [0.0, 0.0, 0.0, 1.0]
+            ]);
+
+            let b = Tuple3::new(1.0, 2.0, 3.0, 1.0);
+
+            assert_eq!(&a * &b, Tuple3::new(18.0, 24.0, 33.0, 1.0));
         }
     }
 }
