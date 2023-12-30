c8e42a8a3a25ce9cfefea05141cbaee4e2aa7ab6
Josh <5685731+marcantony@users.noreply.github.com>
2023-12-29 23:16:55 -0500

    Improve floating point comparison to handle near-zero

--- src/math/util.rs
+++ src/math/util.rs
@@ -1,14 +1,20 @@
 const MAX_ULPS_DIFF: u64 = 8;
+const MAX_ABS_DIFF: f64 = f64::EPSILON;
 
 pub fn are_equal(a: f64, b: f64) -> bool {
     if !a.is_finite() || !b.is_finite() {
         false
     } else {
-        let a_u = a.to_bits();
-        let b_u = b.to_bits();
+        let abs_diff = f64::abs(a - b);
+        if abs_diff <= MAX_ABS_DIFF {
+            true
+        } else {
+            let a_u = a.to_bits();
+            let b_u = b.to_bits();
 
-        let ulps_diff = a_u.abs_diff(b_u);
+            let ulps_diff = a_u.abs_diff(b_u);
 
-        ulps_diff <= MAX_ULPS_DIFF
+            ulps_diff <= MAX_ULPS_DIFF
+        }
     }
 }
