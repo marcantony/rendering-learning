f5c58325d7a030b6e2607907ee63a3fce4998e27
Josh <5685731+marcantony@users.noreply.github.com>
2023-12-29 23:16:43 -0500

    Implement rotation matrices

--- src/math/transformation.rs
+++ src/math/transformation.rs
@@ -18,6 +18,36 @@ pub fn scaling(x: f64, y: f64, z: f64) -> Matrix<4> {
     ])
 }
 
+pub fn rotation_x(radians: f64) -> Matrix<4> {
+    let (s, c) = f64::sin_cos(radians);
+    Matrix::new([
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, c, -s, 0.0],
+        [0.0, s, c, 0.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ])
+}
+
+pub fn rotation_y(radians: f64) -> Matrix<4> {
+    let (s, c) = f64::sin_cos(radians);
+    Matrix::new([
+        [c, 0.0, s, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [-s, 0.0, c, 0.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ])
+}
+
+pub fn rotation_z(radians: f64) -> Matrix<4> {
+    let (s, c) = f64::sin_cos(radians);
+    Matrix::new([
+        [c, -s, 0.0, 0.0],
+        [s, c, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0]
+    ])
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -87,6 +117,51 @@ mod tests {
             let p = Tuple3::point(2.0, 3.0, 4.0);
 
             assert_eq!(&transform * &p, Tuple3::point(-2.0, 3.0, 4.0));
+        }
+    }
+
+    mod rotation {
+        use super::*;
+
+        use std::f64;
+
+        #[test]
+        fn rotating_point_around_x_axis() {
+            let p = Tuple3::point(0.0, 1.0, 0.0);
+            let half_quarter = rotation_x(f64::consts::FRAC_PI_4);
+            let full_quarter = rotation_x(f64::consts::FRAC_PI_2);
+
+            assert_eq!(&half_quarter * &p, Tuple3::point(0.0, f64::consts::SQRT_2 / 2.0, f64::consts::SQRT_2 / 2.0));
+            assert_eq!(&full_quarter * &p, Tuple3::point(0.0, 0.0, 1.0));
+        }
+
+        #[test]
+        fn inverse_of_x_rotation_rotates_in_opposite_direction() {
+            let p = Tuple3::point(0.0, 1.0, 0.0);
+            let half_quarter = rotation_x(f64::consts::FRAC_PI_4);
+            let inv = half_quarter.invert().unwrap();
+
+            assert_eq!(&inv * &p, Tuple3::point(0.0, f64::consts::SQRT_2 / 2.0, -f64::consts::SQRT_2 / 2.0));
+        }
+
+        #[test]
+        fn rotating_point_around_y_axis() {
+            let p = Tuple3::point(0.0, 0.0, 1.0);
+            let half_quarter = rotation_y(f64::consts::FRAC_PI_4);
+            let full_quarter = rotation_y(f64::consts::FRAC_PI_2);
+
+            assert_eq!(&half_quarter * &p, Tuple3::point(f64::consts::SQRT_2 / 2.0, 0.0, f64::consts::SQRT_2 / 2.0));
+            assert_eq!(&full_quarter * &p, Tuple3::point(1.0, 0.0, 0.0));
+        }
+
+        #[test]
+        fn rotating_point_around_z_axis() {
+            let p = Tuple3::point(0.0, 1.0, 0.0);
+            let half_quarter = rotation_z(f64::consts::FRAC_PI_4);
+            let full_quarter = rotation_z(f64::consts::FRAC_PI_2);
+
+            assert_eq!(&half_quarter * &p, Tuple3::point(-f64::consts::SQRT_2 / 2.0, f64::consts::SQRT_2 / 2.0, 0.0));
+            assert_eq!(&full_quarter * &p, Tuple3::point(-1.0, 0.0, 0.0));
         }
     }
 }
