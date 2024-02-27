29c89ef24a445c386b6ed410e6ac88f1c2a640b7
Josh <5685731+marcantony@users.noreply.github.com>
2024-02-26 20:05:05 -0500

    Copy implementation of Transformed Shape for Transformed Object

--- src/scene/object/mod.rs
+++ src/scene/object/mod.rs
@@ -1,6 +1,6 @@
 use crate::{
     draw::color::Color,
-    math::{point::Point3d, vector::NormalizedVec3d},
+    math::{matrix::InvertibleMatrix, point::Point3d, vector::NormalizedVec3d},
 };
 
 use self::bounded::Bounds;
@@ -25,7 +25,10 @@ pub trait Shape {
 pub enum Object<T: Shape + ?Sized> {
     Shape(Box<T>),
     Group,
-    Transformed,
+    Transformed {
+        transform: InvertibleMatrix<4>,
+        object: Box<Self>,
+    },
     Bounded,
 }
 
@@ -34,7 +37,7 @@ impl<T: Shape + ?Sized> Object<T> {
         match &self {
             Object::Shape(s) => s.material(),
             Object::Group => todo!(),
-            Object::Transformed => todo!(),
+            Object::Transformed { object, .. } => object.material(),
             Object::Bounded => todo!(),
         }
     }
@@ -43,7 +46,10 @@ impl<T: Shape + ?Sized> Object<T> {
         match &self {
             Object::Shape(s) => s.color_at(point),
             Object::Group => todo!(),
-            Object::Transformed => todo!(),
+            Object::Transformed { transform, object } => {
+                let transformed_point = transform.inverse() * point;
+                object.color_at(&transformed_point)
+            }
             Object::Bounded => todo!(),
         }
     }
@@ -56,7 +62,13 @@ impl<T: Shape + ?Sized> Object<T> {
                 .map(|i| Intersection::new(i.t(), self))
                 .collect(),
             Object::Group => todo!(),
-            Object::Transformed => todo!(),
+            Object::Transformed { transform, object } => {
+                let local_ray = ray.transform(&transform.inverse());
+                let xs = object.intersect(&local_ray);
+                xs.into_iter()
+                    .map(|x| Intersection::new(x.t(), self))
+                    .collect()
+            }
             Object::Bounded => todo!(),
         }
     }
@@ -65,7 +77,12 @@ impl<T: Shape + ?Sized> Object<T> {
         match &self {
             Object::Shape(s) => s.normal_at(point),
             Object::Group => todo!(),
-            Object::Transformed => todo!(),
+            Object::Transformed { transform, object } => {
+                let local_point = transform.inverse() * point;
+                let local_normal = object.normal_at(&local_point);
+                let world_normal = &transform.inverse().transpose() * &*local_normal;
+                NormalizedVec3d::try_from(world_normal).unwrap()
+            }
             Object::Bounded => todo!(),
         }
     }
@@ -74,9 +91,22 @@ impl<T: Shape + ?Sized> Object<T> {
         match &self {
             Object::Shape(s) => s.bounds(),
             Object::Group => todo!(),
-            Object::Transformed => todo!(),
+            Object::Transformed { .. } => todo!(),
             Object::Bounded => todo!(),
         }
+    }
+
+    pub fn transform(self, transform: InvertibleMatrix<4>) -> Self {
+        Object::Transformed {
+            transform,
+            object: Box::new(self),
+        }
+    }
+}
+
+impl<T: Shape> From<T> for Object<T> {
+    fn from(value: T) -> Self {
+        Object::Shape(Box::new(value))
     }
 }
 
@@ -225,6 +255,179 @@ mod object_tests {
             } else {
                 panic!("broke the test");
             };
+        }
+    }
+
+    mod transformed {
+        use crate::scene::object::test_utils::MockObject;
+
+        use super::*;
+
+        #[test]
+        fn material_of_transformed_object_is_material_of_child() {
+            let shape = MockObject::default();
+            let object = Object::from(shape);
+            let transformed = object.transform(Default::default());
+
+            if let Object::Transformed { object: o, .. } = &transformed {
+                assert!(transformed.material() == o.material());
+            } else {
+                unreachable!();
+            }
+        }
+
+        mod intersect {
+            use crate::{math::vector::Vec3d, scene::transformation};
+
+            use super::*;
+
+            #[test]
+            fn intersecting_scaled_shape_with_ray() {
+                let r = Ray::new(Point3d::new(0.0, 0.0, -5.0), Vec3d::new(0.0, 0.0, 1.0));
+                let s = Object::from(MockObject {
+                    intersect_local_arg_expectation: Some(Ray::new(
+                        Point3d::new(0.0, 0.0, -2.5),
+                        Vec3d::new(0.0, 0.0, 0.5),
+                    )),
+                    ..Default::default()
+                })
+                .transform(
+                    InvertibleMatrix::try_from(transformation::scaling(2.0, 2.0, 2.0)).unwrap(),
+                );
+
+                s.intersect(&r);
+            }
+
+            #[test]
+            fn intersecting_translated_shape_with_ray() {
+                let r = Ray::new(Point3d::new(0.0, 0.0, -5.0), Vec3d::new(0.0, 0.0, 1.0));
+                let s = Object::from(MockObject {
+                    intersect_local_arg_expectation: Some(Ray::new(
+                        Point3d::new(-5.0, 0.0, -5.0),
+                        Vec3d::new(0.0, 0.0, 1.0),
+                    )),
+                    ..Default::default()
+                })
+                .transform(
+                    InvertibleMatrix::try_from(transformation::translation(5.0, 0.0, 0.0)).unwrap(),
+                );
+
+                s.intersect(&r);
+            }
+
+            #[test]
+            fn a_twice_transformed_shape_should_apply_the_inner_transformation_first() {
+                let r = Ray::new(Point3d::new(0.0, 0.0, 0.0), Vec3d::new(0.0, 0.0, 1.0));
+                let t1 = Object::from(MockObject {
+                    intersect_local_arg_expectation: Some(Ray::new(
+                        Point3d::new(-10.0, 0.0, 0.0),
+                        Vec3d::new(0.0, 0.0, 0.5),
+                    )),
+                    ..Default::default()
+                })
+                .transform(
+                    InvertibleMatrix::try_from(transformation::translation(5.0, 0.0, 0.0)).unwrap(),
+                );
+                let t2 = t1.transform(
+                    InvertibleMatrix::try_from(transformation::scaling(2.0, 2.0, 2.0)).unwrap(),
+                );
+
+                t2.intersect(&r);
+            }
+        }
+
+        mod normal_at {
+            use crate::{
+                math::vector::{self, Vec3d},
+                scene::transformation,
+            };
+
+            use super::*;
+
+            #[test]
+            fn computing_normal_on_translated_shape() {
+                let s = Object::from(MockObject::default()).transform(
+                    InvertibleMatrix::try_from(transformation::translation(0.0, 1.0, 0.0)).unwrap(),
+                );
+
+                let n = s.normal_at(&Point3d::new(0.0, 1.70711, -0.70711));
+
+                vector::test_utils::assert_vec_approx_equals(
+                    &n,
+                    &Vec3d::new(0.0, 0.70711, -0.70711),
+                );
+            }
+
+            #[test]
+            fn computing_normal_on_transformed_shape() {
+                let s = Object::from(MockObject::default()).transform(
+                    InvertibleMatrix::try_from(transformation::sequence(&vec![
+                        transformation::rotation_z(std::f64::consts::PI / 5.0),
+                        transformation::scaling(1.0, 0.5, 1.0),
+                    ]))
+                    .unwrap(),
+                );
+
+                let t = std::f64::consts::SQRT_2 / 2.0;
+                let n = s.normal_at(&Point3d::new(0.0, t, -t));
+
+                vector::test_utils::assert_vec_approx_equals(
+                    &n,
+                    &Vec3d::new(0.0, 0.97014, -0.24254),
+                );
+            }
+        }
+
+        mod color_at {
+            use crate::scene::material::Surface;
+            use crate::scene::{pattern::test_utils::MockPattern, transformation};
+
+            use super::*;
+
+            #[test]
+            fn color_at_with_an_object_transformation() {
+                let pattern = MockPattern {
+                    transform: Default::default(),
+                };
+                let shape = Object::from(MockObject {
+                    material: Material {
+                        surface: Surface::Pattern(Box::new(pattern)),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .transform(
+                    InvertibleMatrix::try_from(transformation::scaling(2.0, 2.0, 2.0)).unwrap(),
+                );
+
+                let c = shape.color_at(&Point3d::new(2.0, 3.0, 4.0));
+
+                assert_eq!(c, Color::new(1.0, 1.5, 2.0));
+            }
+
+            #[test]
+            fn color_at_with_an_object_transformation_and_a_pattern_transformation() {
+                let pattern = MockPattern {
+                    transform: InvertibleMatrix::try_from(transformation::translation(
+                        0.5, 1.0, 1.5,
+                    ))
+                    .unwrap(),
+                };
+                let shape = Object::from(MockObject {
+                    material: Material {
+                        surface: Surface::Pattern(Box::new(pattern)),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .transform(
+                    InvertibleMatrix::try_from(transformation::scaling(2.0, 2.0, 2.0)).unwrap(),
+                );
+
+                let c = shape.color_at(&Point3d::new(2.5, 3.0, 3.5));
+
+                assert_eq!(c, Color::new(0.75, 0.5, 0.25));
+            }
         }
     }
 }
