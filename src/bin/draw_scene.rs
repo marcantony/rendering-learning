e0b66e3b624dd21b3c607a9954d177aca27bb4b6
Josh <5685731+marcantony@users.noreply.github.com>
2024-03-15 21:50:29 -0400

    Create hexagon from groups

--- src/bin/draw_scene.rs
+++ src/bin/draw_scene.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::f64::consts;
 
 use ray_tracer_challenge::{
@@ -8,7 +10,8 @@ use ray_tracer_challenge::{
         light::PointLight,
         material::{Material, Surface},
         object::{
-            cube::Cube, group::Group, plane::Plane, sphere::Sphere, transformed::Transformed,
+            bounded::Bounded, cube::Cube, cylinder::Cylinder, group::Group, plane::Plane,
+            sphere::Sphere, transformed::Transformed, Object,
         },
         pattern::{checker3d::Checker3d, stripe::Stripe},
         transformation,
@@ -24,6 +27,98 @@ fn main() {
     use std::time::Instant;
     let now = Instant::now();
 
+    let world = test_hexagon_world();
+
+    let from = Point3d::new(0.0, 2.0, -7.0);
+    let to = Point3d::new(0.0, 1.5, 0.0);
+    let up = Vec3d::new(0.0, 1.0, 0.0);
+    let camera = Camera::new(
+        RES_X,
+        RES_Y,
+        consts::FRAC_PI_3,
+        InvertibleMatrix::try_from(transformation::view_transform(&from, &to, &up)).unwrap(),
+    );
+
+    println!("Rendering scene...");
+    let canvas = camera.render(&world);
+    println!("Scene rendered.");
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    util::write_to_file(&canvas, "scene");
+}
+
+fn hexagon_corner() -> impl Object {
+    let corner = Sphere::unit();
+    let transform = transformation::sequence(&vec![
+        transformation::scaling(0.25, 0.25, 0.25),
+        transformation::translation(0.0, 0.0, -1.0),
+    ]);
+    Transformed::new(corner, InvertibleMatrix::try_from(transform).unwrap())
+}
+
+fn hexagon_edge() -> impl Object {
+    let edge = Cylinder {
+        minimum: Some(0.0),
+        maximum: Some(1.0),
+        ..Default::default()
+    };
+    let transform = transformation::sequence(&vec![
+        transformation::scaling(0.25, 1.0, 0.25),
+        transformation::rotation_z(-std::f64::consts::FRAC_PI_2),
+        transformation::rotation_y(-std::f64::consts::FRAC_PI_6),
+        transformation::translation(0.0, 0.0, -1.0),
+    ]);
+    Transformed::new(edge, InvertibleMatrix::try_from(transform).unwrap())
+}
+
+fn hexagon_side() -> impl Object {
+    Bounded::new(Group::new(vec![
+        Box::new(hexagon_corner()),
+        Box::new(hexagon_edge()),
+    ]))
+}
+
+fn hexagon() -> impl Object {
+    let sides: Vec<_> = (0..6)
+        .map(|n| {
+            let transform = transformation::rotation_y(n as f64 * std::f64::consts::FRAC_PI_3);
+            Transformed::new(
+                hexagon_side(),
+                InvertibleMatrix::try_from(transform).unwrap(),
+            )
+        })
+        .map(|s| Box::new(s) as Box<dyn Object>)
+        .collect();
+
+    Group::new(sides)
+}
+
+fn test_hexagon_world() -> World {
+    let light_source = PointLight {
+        position: Point3d::new(-10.0, 10.0, -10.0),
+        intensity: color::white(),
+    };
+
+    let hexagon = Transformed::new(
+        hexagon(),
+        InvertibleMatrix::try_from(transformation::sequence(&vec![
+            transformation::scaling(2.0, 2.0, 2.0),
+            transformation::rotation_x(-std::f64::consts::FRAC_PI_4),
+            transformation::translation(0.0, 2.0, 0.0),
+        ]))
+        .unwrap(),
+    );
+
+    World {
+        objects: vec![Box::new(hexagon)],
+        lights: vec![light_source],
+        ..Default::default()
+    }
+}
+
+fn test_mirror_world() -> World {
     let gs1 = Transformed::new(
         Sphere::unit(),
         InvertibleMatrix::try_from(transformation::translation(-0.5, 0.0, 0.0)).unwrap(),
@@ -32,10 +127,10 @@ fn main() {
         Sphere::unit(),
         InvertibleMatrix::try_from(transformation::translation(0.5, 0.0, 0.0)).unwrap(),
     );
-    let sphere_group = Transformed::new(
+    let sphere_group = Bounded::new(Transformed::new(
         Group::new(vec![Box::new(gs1), Box::new(gs2)]),
         InvertibleMatrix::try_from(transformation::translation(-1.0, 0.0, 0.0)).unwrap(),
-    );
+    ));
 
     let floor = Plane {
         material: Material {
@@ -176,7 +271,7 @@ fn main() {
         intensity: color::white(),
     };
 
-    let world = World {
+    World {
         objects: vec![
             Box::new(floor),
             Box::new(left_wall),
@@ -190,24 +285,5 @@ fn main() {
         ],
         lights: vec![light_source],
         ..Default::default()
-    };
-
-    let from = Point3d::new(0.0, 2.0, -7.0);
-    let to = Point3d::new(0.0, 1.5, 0.0);
-    let up = Vec3d::new(0.0, 1.0, 0.0);
-    let camera = Camera::new(
-        RES_X,
-        RES_Y,
-        consts::FRAC_PI_3,
-        InvertibleMatrix::try_from(transformation::view_transform(&from, &to, &up)).unwrap(),
-    );
-
-    println!("Rendering scene...");
-    let canvas = camera.render(&world);
-    println!("Scene rendered.");
-
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
-
-    util::write_to_file(&canvas, "scene");
+    }
 }
