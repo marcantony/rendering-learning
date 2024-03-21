f2a4918c25e13de882f226be63e9a049d00d9967
Josh <5685731+marcantony@users.noreply.github.com>
2024-03-20 22:45:17 -0400

    Parse vertex normals and faces with normals in OBJ files

--- src/io/wavefront_obj.rs
+++ src/io/wavefront_obj.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, io::BufRead};
 
 use crate::{
-    math::point::Point3d,
+    math::{point::Point3d, vector::Vec3d},
     scene::object::{bounded::Bounded, group::Group, triangle::Triangle, Object},
 };
 
@@ -15,6 +15,7 @@ pub struct WavefrontObj {
     ignored: u32,
     groups: HashMap<ObjGroup, Vec<Triangle>>,
     vertices: Vec<Point3d>,
+    normals: Vec<Vec3d>,
 }
 
 impl WavefrontObj {
@@ -25,29 +26,37 @@ impl WavefrontObj {
             ignored: 0,
             groups: HashMap::new(),
             vertices: Vec::new(),
+            normals: Vec::new(),
         };
 
         let mut current_group_name = ObjGroup::Default;
         let mut current_group_val = Vec::<Triangle>::new();
         for line in lines_iter {
             let split = line.split_once(' ');
-            let status: Option<()> = split.and_then(|(head, tail)| match head {
-                "v" => {
-                    let res = parse_vertex(tail);
-                    res.map(|p| obj.vertices.push(p))
+            let status: Option<()> = split.and_then(|(head, tail)| {
+                let trimmed = tail.trim();
+                match head {
+                    "v" => {
+                        let res = parse_vertex(trimmed);
+                        res.map(|p| obj.vertices.push(p))
+                    }
+                    "f" => {
+                        let res = parse_face(trimmed, &obj.vertices, &obj.normals);
+                        res.map(|mut ts| current_group_val.append(&mut ts))
+                    }
+                    "g" => {
+                        let group_vals = current_group_val.drain(..).collect::<Vec<_>>();
+                        obj.groups.insert(current_group_name.clone(), group_vals);
+                        current_group_name = ObjGroup::Named(String::from(trimmed));
+                        current_group_val.clear();
+                        Some(())
+                    }
+                    "vn" => {
+                        let res = parse_normal(trimmed);
+                        res.map(|n| obj.normals.push(n))
+                    }
+                    _ => None,
                 }
-                "f" => {
-                    let res = parse_face(tail, &obj.vertices);
-                    res.map(|mut ts| current_group_val.append(&mut ts))
-                }
-                "g" => {
-                    let group_vals = current_group_val.drain(..).collect::<Vec<_>>();
-                    obj.groups.insert(current_group_name.clone(), group_vals);
-                    current_group_name = ObjGroup::Named(String::from(tail));
-                    current_group_val.clear();
-                    Some(())
-                }
-                _ => None,
             });
 
             match status {
@@ -81,31 +90,82 @@ fn parse_vertex(tail: &str) -> Option<Point3d> {
     })
 }
 
-fn parse_face(tail: &str, vertices: &[Point3d]) -> Option<Vec<Triangle>> {
+fn parse_normal(tail: &str) -> Option<Vec3d> {
     let nums = tail
         .split_whitespace()
-        .map(|s| s.parse::<usize>().ok())
-        .collect::<Option<Vec<_>>>();
+        .map(|s| s.parse::<f64>().ok())
+        .collect::<Option<Vec<f64>>>();
 
     nums.and_then(|ns| {
-        ns.iter()
-            .map(|n| vertices.get(n - 1))
-            .map(|p| p.cloned())
-            .collect::<Option<Vec<_>>>()
+        if ns.len() == 3 {
+            Some(Vec3d::new(ns[0], ns[1], ns[2]))
+        } else {
+            None
+        }
     })
-    .and_then(|points| {
-        if points.len() == 3 {
-            points
-                .try_into()
-                .ok()
-                .map(|ps: [Point3d; 3]| vec![Triangle::flat(ps, Default::default())])
-        } else if points.len() > 3 {
-            let triangulated = fan_triangulate(&points);
+}
+
+fn parse_face(
+    tail: &str,
+    read_vertices: &[Point3d],
+    read_normals: &[Vec3d],
+) -> Option<Vec<Triangle>> {
+    let tokens = tail.split_whitespace();
+
+    let indices: Option<Vec<(usize, Option<usize>)>> = tokens
+        .map(|token| {
+            let indices = token.split('/').collect::<Vec<_>>();
+            let v_n_indices_unparsed = match indices.len() {
+                1 => Some((indices[0], None)),
+                2 => Some((indices[0], None)),
+                3 => Some((indices[0], Some(indices[2]))),
+                _ => None,
+            };
+
+            v_n_indices_unparsed.and_then(|(v_unparsed, n_unparsed)| {
+                let v_parsed = v_unparsed.parse::<usize>().ok();
+                match n_unparsed {
+                    Some(val) => {
+                        let n_parsed = val.parse::<usize>().ok();
+                        n_parsed.and_then(|n| v_parsed.map(|v| (v, Some(n))))
+                    }
+                    None => v_parsed.map(|v| (v, None)),
+                }
+            })
+        })
+        .collect();
+
+    let mapped_indices = indices.map(|ns| {
+        ns.iter()
+            .map(|(vi, ni)| (&read_vertices[vi - 1], ni.map(|n| &read_normals[n - 1])))
+            .collect::<Vec<_>>()
+    });
+
+    mapped_indices.and_then(|vertices| {
+        if vertices.len() >= 3 {
+            let triangulated = fan_triangulate(&vertices);
             Some(
                 triangulated
                     .into_iter()
-                    .map(|verts| verts.map(|v| v.clone()))
-                    .map(|verts| Triangle::flat(verts, Default::default()))
+                    .map(|verts| {
+                        let points = [verts[0].0, verts[1].0, verts[2].0].map(|p| p.clone());
+                        let normals = verts[0].1.and_then(|first| {
+                            verts[1].1.and_then(|second| {
+                                verts[2]
+                                    .1
+                                    .map(|third| [first, second, third].map(|n| n.clone()))
+                            })
+                        });
+
+                        match normals {
+                            Some(ns) => {
+                                let [p1, p2, p3] = points;
+                                let [n1, n2, n3] = ns;
+                                Triangle::smooth([(p1, n1), (p2, n2), (p3, n3)], Default::default())
+                            }
+                            None => Triangle::flat(points, Default::default()),
+                        }
+                    })
                     .collect::<Vec<_>>(),
             )
         } else {
@@ -114,11 +174,13 @@ fn parse_face(tail: &str, vertices: &[Point3d]) -> Option<Vec<Triangle>> {
     })
 }
 
-fn fan_triangulate(vertices: &[Point3d]) -> Vec<[&Point3d; 3]> {
-    let mut triangles = Vec::<[&Point3d; 3]>::new();
+fn fan_triangulate<'a>(
+    vertices: &[(&'a Point3d, Option<&'a Vec3d>)],
+) -> Vec<[(&'a Point3d, Option<&'a Vec3d>); 3]> {
+    let mut triangles = Vec::<[(&Point3d, Option<&Vec3d>); 3]>::new();
 
     for i in 2..vertices.len() {
-        triangles.push([&vertices[0], &vertices[i - 1], &vertices[i]])
+        triangles.push([vertices[0], vertices[i - 1], vertices[i]])
     }
 
     triangles
@@ -129,6 +191,8 @@ mod tests {
     use super::*;
 
     mod parse {
+        use crate::math::vector::Vec3d;
+
         use super::*;
 
         #[test]
@@ -306,6 +370,61 @@ f 1 3 4
                     Default::default()
                 )
             );
+        }
+
+        #[test]
+        fn vertex_normal_records() {
+            let data = "
+vn 0 0 1
+vn 0.707 0 -0.707
+vn 1 2 3
+";
+
+            let parsed = WavefrontObj::parse(data.as_bytes());
+
+            assert_eq!(
+                parsed.normals,
+                [
+                    Vec3d::new(0.0, 0.0, 1.0),
+                    Vec3d::new(0.707, 0.0, -0.707),
+                    Vec3d::new(1.0, 2.0, 3.0),
+                ]
+            )
+        }
+
+        #[test]
+        fn faces_with_normals() {
+            let data = "
+v 0 1 0
+v -1 0 0
+v 1 0 0
+
+vn -1 0 0
+vn 1 0 0
+vn 0 1 0
+
+f 1//3 2//1 3//2
+f 1/0/3 2/102/1 3/14/2
+";
+
+            let parsed = WavefrontObj::parse(data.as_bytes());
+
+            let g = parsed.groups.get(&ObjGroup::Default).unwrap();
+            let t1 = &g[0];
+            let t2 = &g[1];
+
+            assert_eq!(
+                t1,
+                &Triangle::smooth(
+                    [
+                        (Point3d::new(0.0, 1.0, 0.0), Vec3d::new(0.0, 1.0, 0.0)),
+                        (Point3d::new(-1.0, 0.0, 0.0), Vec3d::new(-1.0, 0.0, 0.0)),
+                        (Point3d::new(1.0, 0.0, 0.0), Vec3d::new(1.0, 0.0, 0.0)),
+                    ],
+                    Default::default()
+                )
+            );
+            assert_eq!(t1, t2);
         }
     }
 
