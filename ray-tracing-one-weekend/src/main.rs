4bf6832762beac367444ef15289c3caeedac62e7
Josh <5685731+marcantony@users.noreply.github.com>
2024-03-31 22:19:38 -0400

    Add progress indication

--- ray-tracing-one-weekend/src/main.rs
+++ ray-tracing-one-weekend/src/main.rs
@@ -11,6 +11,7 @@ fn main() -> Result<()> {
     writeln!(writer, "255")?;
 
     for j in 0..image_height {
+        eprintln!("Scanlines remaining: {}", (image_height - j));
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -23,6 +24,8 @@ fn main() -> Result<()> {
             writeln!(writer, "{} {} {}", ir, ig, ib)?
         }
     }
+
+    eprintln!("Done.");
 
     Ok(())
 }
