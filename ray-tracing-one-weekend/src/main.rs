833d4bf69ed2af87103937c861d0dce2504d431e
Josh <5685731+marcantony@users.noreply.github.com>
2024-03-31 22:12:38 -0400

    Write out ppm file
    Done a little differently than in The Ray Tracer Challenge :)

--- ray-tracing-one-weekend/src/main.rs
+++ ray-tracing-one-weekend/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufWriter, Result, Write};
+
+fn main() -> Result<()> {
+    let image_width: usize = 256;
+    let image_height: usize = 256;
+
+    let mut writer = BufWriter::new(io::stdout().lock());
+
+    writeln!(writer, "P3")?;
+    writeln!(writer, "{} {}", image_width, image_height)?;
+    writeln!(writer, "255")?;
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0 as f64;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            writeln!(writer, "{} {} {}", ir, ig, ib)?
+        }
+    }
+
+    Ok(())
 }
