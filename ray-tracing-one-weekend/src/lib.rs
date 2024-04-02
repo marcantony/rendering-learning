03da09f3b47a4bc2af6616860e1ba80ad8415805
Josh <5685731+marcantony@users.noreply.github.com>
2024-04-01 22:17:50 -0400

    Create ray type

--- ray-tracing-one-weekend/src/lib.rs
+++ ray-tracing-one-weekend/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod color;
+pub mod ray;
 pub mod vec3;
---
+++ ray-tracing-one-weekend/src/ray.rs
@@ -0,0 +1,43 @@
+use crate::vec3::{Point3, Vec3};
+
+#[derive(Debug, Clone)]
+pub struct Ray {
+    pub origin: Point3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Ray { origin, direction }
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        &self.origin + &(&self.direction * t)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use float_cmp::{assert_approx_eq, ApproxEq, F64Margin};
+
+    use super::*;
+
+    impl ApproxEq for &Ray {
+        type Margin = F64Margin;
+
+        fn approx_eq<M: Into<Self::Margin>>(self, other: Self, margin: M) -> bool {
+            let m = margin.into();
+            self.origin.approx_eq(&other.origin, m) && self.direction.approx_eq(&other.direction, m)
+        }
+    }
+
+    #[test]
+    fn compute_a_point_from_a_distance() {
+        let r = Ray::new(Point3::new(2.0, 3.0, 4.0), Vec3::new(1.0, 0.0, 0.0));
+
+        assert_approx_eq!(&Point3, &r.at(0.0), &Point3::new(2.0, 3.0, 4.0));
+        assert_approx_eq!(&Point3, &r.at(1.0), &Point3::new(3.0, 3.0, 4.0));
+        assert_approx_eq!(&Point3, &r.at(-1.0), &Point3::new(1.0, 3.0, 4.0));
+        assert_approx_eq!(&Point3, &r.at(2.5), &Point3::new(4.5, 3.0, 4.0));
+    }
+}
