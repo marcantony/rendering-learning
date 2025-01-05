be5159d2bfac00da61cb1ecb4633ad883805c153
Josh <5685731+marcantony@users.noreply.github.com>
2025-01-05 18:48:28 -0500

    Use random vectors to shift perlin values off integer values

--- ray-tracing-one-weekend/src/perlin.rs
+++ ray-tracing-one-weekend/src/perlin.rs
@@ -1,13 +1,13 @@
 use rand::Rng;
 
-use crate::vec3::Point3;
+use crate::vec3::{Point3, Vec3};
 
 const BIT_COUNT: usize = 8;
 const POINT_COUNT: usize = (2 as usize).pow(BIT_COUNT as u32);
 const INDEX_BITMASK: i32 = POINT_COUNT as i32 - 1;
 
 pub struct Perlin {
-    randfloat: [f64; POINT_COUNT],
+    randvec: [Vec3; POINT_COUNT],
     perm_x: [usize; POINT_COUNT],
     perm_y: [usize; POINT_COUNT],
     perm_z: [usize; POINT_COUNT],
@@ -15,8 +15,7 @@ pub struct Perlin {
 
 impl Perlin {
     pub fn new(rand: &mut impl Rng) -> Self {
-        let mut randfloat = [0f64; POINT_COUNT];
-        rand.fill(&mut randfloat);
+        let randvec = std::array::from_fn(|_i| Vec3::random_unit_vector(rand));
 
         let mut perm_x = std::array::from_fn(|i| i);
         permute(&mut perm_x, rand);
@@ -28,32 +27,33 @@ impl Perlin {
         permute(&mut perm_z, rand);
 
         Perlin {
-            randfloat,
+            randvec,
             perm_x,
             perm_y,
             perm_z,
         }
     }
 
+    /// Outputs a noise value for the given point in the range [-1, 1]
     pub fn noise(&self, p: &Point3) -> f64 {
         let u = p.x() - p.x().floor();
         let v = p.y() - p.y().floor();
         let w = p.z() - p.z().floor();
 
-        let u = u * u * (3.0 - 2.0 * u);
-        let v = v * v * (3.0 - 2.0 * v);
-        let w = w * w * (3.0 - 2.0 * w);
-
         let i = p.x().floor() as i32;
         let j = p.y().floor() as i32;
         let k = p.z().floor() as i32;
 
-        let mut c = [[[0f64; 2]; 2]; 2];
+        // This array initialization could probably use some cleaning up
+        let vec = Vec3::zero();
+        let vecs = std::array::from_fn(|_i| &vec);
+        let vecs3 = std::array::from_fn(|_i| vecs.clone());
+        let mut c: [[[&Vec3; 2]; 2]; 2] = std::array::from_fn(|_i| vecs3.clone());
 
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
-                    c[di][dj][dk] = self.randfloat[self.perm_x
+                    c[di][dj][dk] = &self.randvec[self.perm_x
                         [((i + di as i32) & INDEX_BITMASK) as usize]
                         ^ self.perm_y[((j + dj as i32) & INDEX_BITMASK) as usize]
                         ^ self.perm_z[((k + dk as i32) & INDEX_BITMASK) as usize]];
@@ -61,7 +61,7 @@ impl Perlin {
             }
         }
 
-        trilinear_interp(&c, u, v, w)
+        perlin_interp(&c, u, v, w)
     }
 }
 
@@ -74,18 +74,26 @@ fn permute(p: &mut [usize], rand: &mut impl Rng) {
     }
 }
 
-fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+fn perlin_interp(c: &[[[&Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let uu = u * u * (3.0 - 2.0 * u);
+    let vv = v * v * (3.0 - 2.0 * v);
+    let ww = w * w * (3.0 - 2.0 * w);
+
     let mut accum = 0.0;
+
     for i in 0..2 as usize {
         for j in 0..2 as usize {
             for k in 0..2 as usize {
                 let i_f = i as f64;
                 let j_f = j as f64;
                 let k_f = k as f64;
-                accum += (i_f * u + (1.0 - i_f) * (1.0 - u))
-                    * (j_f * v + (1.0 - j_f) * (1.0 - v))
-                    * (k_f * w + (1.0 - k_f) * (1.0 - w))
-                    * c[i][j][k]
+
+                let weight_v = Vec3::new(u - i_f, v - j_f, w - k_f);
+
+                accum += (i_f * uu + (1.0 - i_f) * (1.0 - uu))
+                    * (j_f * vv + (1.0 - j_f) * (1.0 - vv))
+                    * (k_f * ww + (1.0 - k_f) * (1.0 - ww))
+                    * c[i][j][k].dot(&weight_v)
             }
         }
     }
--- ray-tracing-one-weekend/src/texture.rs
+++ ray-tracing-one-weekend/src/texture.rs
@@ -88,6 +88,6 @@ pub struct Noise {
 
 impl Texture for Noise {
     fn value(&self, _u: f64, _v: f64, p: &Point3) -> Color {
-        Color::new(1.0, 1.0, 1.0) * self.noise.noise(&(self.scale * p))
+        Color::new(1.0, 1.0, 1.0) * 0.5 * (1.0 + self.noise.noise(&(self.scale * p)))
     }
 }
