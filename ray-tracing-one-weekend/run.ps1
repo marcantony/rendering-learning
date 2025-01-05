9b1057bc5bfb6f5df6516a6b6eb2253a18c6b856
Josh <5685731+marcantony@users.noreply.github.com>
2025-01-04 23:07:33 -0500

    Add new dev-raytrace profile to use by default for running examples
    dev-raytrace has more optimization than dev so it should run faster,
    but less optimization than release so it should compile faster.

--- ray-tracing-one-weekend/run.ps1
+++ ray-tracing-one-weekend/run.ps1
@@ -1,5 +1,6 @@
 param (
-    [Parameter(Mandatory=$true)][string]$example
+    [Parameter(Mandatory=$true)][string]$example,
+    [string]$profile = "dev-raytrace"
 )
 
-cargo run --release --package ray-tracing-one-weekend --example $example | set-content output/$example-$(Get-Date -UFormat %s -Millisecond 0).ppm -encoding String
+cargo run --profile $profile --package ray-tracing-one-weekend --example $example | set-content output/$example-$(Get-Date -UFormat %s -Millisecond 0).ppm -encoding String
