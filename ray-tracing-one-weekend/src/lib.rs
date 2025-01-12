daabbb5a61e0bf0ab1a2fb98d45eb99e001e2769
Josh <5685731+marcantony@users.noreply.github.com>
2025-01-12 00:19:53 -0500

    Add 3x3 matrices for refactoring transformations

--- ray-tracing-one-weekend/src/lib.rs
+++ ray-tracing-one-weekend/src/lib.rs
@@ -6,6 +6,7 @@ pub mod hittable;
 pub mod interval;
 pub mod io;
 pub mod material;
+pub mod matrix;
 pub mod output;
 pub mod perlin;
 pub mod ray;
---
+++ ray-tracing-one-weekend/src/matrix.rs
@@ -0,0 +1,72 @@
+use std::ops::Mul;
+
+use crate::vec3::Vec3;
+
+#[derive(Debug, PartialEq)]
+pub struct Matrix3(pub [[f64; 3]; 3]);
+
+impl Mul<&Matrix3> for &Matrix3 {
+    type Output = Matrix3;
+
+    fn mul(self, rhs: &Matrix3) -> Self::Output {
+        let mut out = [[0.0; 3]; 3];
+
+        for n in 0..3 {
+            for m in 0..3 {
+                let mut sum = 0.0;
+                for k in 0..3 {
+                    sum += self.0[n][k] * rhs.0[k][m];
+                }
+                out[n][m] = sum;
+            }
+        }
+
+        Matrix3(out)
+    }
+}
+
+impl Mul<&Vec3> for &Matrix3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: &Vec3) -> Self::Output {
+        let mut out = [0.0; 3];
+
+        let vec_data = [rhs.x(), rhs.y(), rhs.z()];
+
+        for n in 0..3 {
+            let mut sum = 0.0;
+            for k in 0..3 {
+                sum += self.0[n][k] * vec_data[k];
+            }
+            out[n] = sum;
+        }
+
+        Vec3::new(out[0], out[1], out[2])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn multiply_two_matrices() {
+        let a = Matrix3([[1.0, 2.0, -1.0], [3.0, 2.0, 0.0], [-4.0, 0.0, 2.0]]);
+
+        let b = Matrix3([[3.0, 4.0, 2.0], [0.0, 1.0, 0.0], [-2.0, 0.0, 1.0]]);
+
+        assert_eq!(
+            &a * &b,
+            Matrix3([[5.0, 6.0, 1.0], [9.0, 14.0, 6.0], [-16.0, -16.0, -6.0]])
+        );
+    }
+
+    #[test]
+    fn multiply_matrix_and_vec() {
+        let a = Matrix3([[1.0, 2.0, -1.0], [3.0, 2.0, 0.0], [-4.0, 0.0, 2.0]]);
+
+        let b = Vec3::new(1.0, 2.0, 3.0);
+
+        assert_eq!(&a * &b, Vec3::new(2.0, 7.0, 2.0));
+    }
+}
